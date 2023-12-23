41484e9518904d6f70e3d073c13efbe03c10e146
FrancoiseSagan <2537953899@qq.com>
2023-12-23 18:46:24 +0800

    服务器搭载版本

--- src/main/java/org/urbcomp/startdb/compress/elf/Main.java
+++ src/main/java/org/urbcomp/startdb/compress/elf/Main.java
@@ -2,14 +2,26 @@ package org.urbcomp.startdb.compress.elf;
 
 import java.io.*;
 
+import static org.urbcomp.startdb.compress.elf.fileoperation.CompressFileOperation.readValuesFromCSV;
 import static org.urbcomp.startdb.compress.elf.fileoperation.DecompressFileOperation.writeDoubleToCSV;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        String csvFilePath = "C:\\Users\\25379\\Downloads\\Stocks-UK.csv";
-        String outputBinFilePath = "fuck.bin";
-        String outputCSVFilePath = "null.csv";
-        writeDoubleToCSV(outputBinFilePath,outputCSVFilePath);
+        int flag = Integer.parseInt(args[0]);
+        String csvFilePath;
+        String binFilePath;
+
+        if(flag == 0){
+            csvFilePath = args[1];
+            binFilePath = args[2];
+            readValuesFromCSV(csvFilePath,binFilePath);
+        }
+
+        else{
+            binFilePath = args[1];
+            csvFilePath = args[2];
+            writeDoubleToCSV(binFilePath,csvFilePath);
+        }
     }
 }
